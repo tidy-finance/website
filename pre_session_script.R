53f8013da9ecc75ac9df0dc6df7efc83f6d09754
voigtstefan <stefan.voigt@econ.ku.dk>
2022-07-15 12:24:02 +0200

    Move color palette to manual workflow

--- pre_session_script.R
+++ pre_session_script.R
@@ -24,8 +24,8 @@ options(tibble.print_max = 5,
 library(ggplot2)
 theme_set(theme_bw() + theme(legend.position = "bottom"))
 
-pal <- colorRampPalette(list("#3B9AB2", "#78B7C5", "#EBCC2A", "#E1AF00", "#F21A00"))
-if(!knitr::is_html_output()) pal <- colorRampPalette(list("#191919", "#ffffff"))
+if(knitr::is_html_output()) pal <- colorRampPalette(list("#3B9AB2", "#78B7C5", "#EBCC2A", "#E1AF00", "#F21A00"))
+if(knitr::is_latex_output()) pal <- colorRampPalette(list("#191919", "#ffffff"))
 
 scale_colour_continuous <- function(...) scale_color_gradientn(colours = pal(256), ...)
 scale_colour_discrete   <- function(...) discrete_scale("colour", scale_name = "pal", palette = pal, ...)
