d947d5725018a99ff27448031495bcb6db0d6783
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2023-11-02 20:18:04 +0100

    Moved imports from mizani from pre_render_script back to introduction-to-tidy-finance

--- python/pre_render_script.py
+++ python/pre_render_script.py
@@ -3,7 +3,3 @@ warnings.simplefilter(action="ignore", category=FutureWarning)
 
 from plotnine import theme_set, theme, theme_bw
 theme_set(theme_bw() + theme(legend_position="bottom"))
-
-from mizani.breaks import date_breaks
-from mizani.formatters import date_format, percent_format
-custom_palette = ["#3B9AB2", "#78B7C5", "#EBCC2A", "#E1AF00", "#F21A00"]
