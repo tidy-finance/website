cf3596ee2f0e44fa98f651c93aaad4f7480c1701
Patrick Weiss <patrick.weiss@wu.ac.at>
2024-01-13 19:01:14 +0000

    Update pre-render and plot config

---
+++ python/plotnine-custom.py
@@ -0,0 +1,14 @@
+theme_set(
+  theme_bw() + theme(
+    legend_position="bottom", 
+    legend_key=element_rect(fill = "white", color = "white"), 
+    title=element_text(size=10), 
+    legend_box_spacing=0,
+    figure_size=(6.4, 4))
+)
+
+class ggplot(ggplot):
+    def __init__(self, data = None, mapping = None, environment = None):
+        super().__init__(data, mapping, environment)
+        self.scales.append(scale_color_grey())
+        self.scales.append(scale_fill_grey())
