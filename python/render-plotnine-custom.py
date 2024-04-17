13c13aeeaf9b516d98a4b01e251e3e42d6efbd09
Patrick Weiss <patrick.weiss@wu.ac.at>
2024-04-17 12:48:26 +0000

    Remove grey-scale

--- python/render-plotnine-custom.py
+++ python/render-plotnine-custom.py
@@ -6,9 +6,3 @@ theme_set(
     legend_box_spacing=0,
     figure_size=(6.4, 4))
 )
-
-class ggplot(ggplot):
-    def __init__(self, data = None, mapping = None, environment = None):
-        super().__init__(data, mapping, environment)
-        self.scales.append(scale_color_grey())
-        self.scales.append(scale_fill_grey())
