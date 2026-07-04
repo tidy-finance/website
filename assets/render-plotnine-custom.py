001651bd732ce8d7c242519936629a316b0c7a6f
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2026-07-04 17:13:32 +0200

    Implement my suggested changes (to be verified)

--- assets/render-plotnine-custom.py
+++
@@ -1,9 +0,0 @@
-from plotnine import theme_set, theme_bw, theme, element_rect
-
-theme_set(
-  theme_bw() + theme(
-    legend_position="bottom",
-    legend_key=element_rect(fill="white", color="white"),
-    legend_box_spacing=0
-  )
-)
